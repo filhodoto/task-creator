8594281197b8678e041de23a4ea60f8c9a77f080
Gonçalo Ramalho <goncalo_ramalho@hotmail.com>
2024-06-07 18:36:06 +0100

    Remove unnecessary prop in  circular loader

--- src/components/CircularLoader/index.tsx
+++ src/components/CircularLoader/index.tsx
@@ -1,11 +1,7 @@
-import { Box, CircularProgress, Typography } from '@mui/material';
-import { SerializedError } from '@reduxjs/toolkit';
-import { FetchBaseQueryError } from '@reduxjs/toolkit/query';
-
+import { CircularProgress } from '@mui/material';
 interface QueryFeedbackProps {
   isLoading: boolean;
   isFetching: boolean;
-  error: SerializedError | FetchBaseQueryError | undefined;
 }
 
 // Component to show user feedback when we make an API query. Created to prevent DRY
